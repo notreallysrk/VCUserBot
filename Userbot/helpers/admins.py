5476148b3a9d97166b96c55be7bfabe2375c4de1
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2021-12-17 13:20:35 +0530

    Update admins.py

--- Userbot/helpers/admins.py
+++ Userbot/helpers/admins.py
@@ -2,8 +2,8 @@ from typing import List
 
 from pyrogram.types import Chat
 
-from MusicAndVideo.helpers.get_admins import get as gett
-from MusicAndVideo.helpers.get_admins import set
+from Userbot.helpers.get_admins import get as gett
+from Userbot.helpers.get_admins import set
 
 
 async def get_administrators(chat: Chat) -> List[int]:
